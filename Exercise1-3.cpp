f850188453fab8a40b1cb72b5bf4faf1d105f389
Adriana-HS <adriana2007hs@gmail.com>
2025-04-05 10:00:16 -0400

    Create Exercise1-3.cpp

---
+++ Exercise1-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {   
+   int width, height, area;
+   cout << "Enter rectangle width and height" << endl;
+   cin >> width >> height;
+   area = width * height;
+   cout << "Rectangle area is " << area << endl;
+
+   return 0;
+}
