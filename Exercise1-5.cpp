52857468460f30582800e45c7511e068715646a9
Adriana-HS <adriana2007hs@gmail.com>
2025-04-05 10:03:16 -0400

    Create Exercise1-5.cpp

---
+++ Exercise1-5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {    
+    double x, y;
+    cout << "Enter 2 integers numbers:" << endl;
+    cin >> x >> y;
+    cout << x + y << endl;
+  
+   	    return 0;
+}
