f650883dd65a4f7d992986a3cba952263a28e81a
Adriana-HS <adriana2007hs@gmail.com>
2025-04-05 09:56:20 -0400

    Create Exercise1-2.cpp

---
+++ Exercise1-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {   
+   double num_1, num_2, resultado_div;
+   cin >> num_1 >> num_2;
+   resultado_div = num_1 / num_2;
+   cout << resultado_div << endl;
+
+   return 0;
+}
