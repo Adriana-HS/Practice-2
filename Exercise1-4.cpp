786ce01644dd0d700196a590667a074764770d6b
Adriana-HS <adriana2007hs@gmail.com>
2025-04-05 10:01:49 -0400

    Create Exercise1-4.cpp

---
+++ Exercise1-4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x, y, z;
+    z = 1;
+    cout << "Enter two entigers numbers: " << endl;
+    cin >> x >> y;
+    cout << "Your answer:" << x + y + z << endl;
+
+    return 0;
+}
