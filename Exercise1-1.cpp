41e0baec70c032e18758f0d4549f2c7aa6351578
Adriana-HS <adriana2007hs@gmail.com>
2025-04-05 09:55:07 -0400

    Create Exercise1-1.cpp

---
+++ Exercise1-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   double num_1, num_2;
+   cin >> num_1 >> num_2;
+   int suma = num_1 + num_2;
+   cout << suma << endl;
+   
+   return 0;
+}
