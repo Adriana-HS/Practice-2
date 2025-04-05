9fe608d71452a0d6d61c84e1522958e3dab3d7d0
Adriana-HS <adriana2007hs@gmail.com>
2025-04-05 10:06:42 -0400

    Create Exercise1-6.cpp

---
+++ Exercise1-6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {    
+    string nombre, apellido;
+    cout << "Ingrese su nombre y apellido paterno:" << endl;
+    cin >> nombre >> apellido;
+    cout << apellido + ", " + nombre << endl;
+  
+   	    return 0;
+}
